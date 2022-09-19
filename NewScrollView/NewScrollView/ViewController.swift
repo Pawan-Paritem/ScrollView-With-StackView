8492f18cf91e168444fb5be54504c253b419b8c5
Pawan-Paritem <72556038+Pawan-Paritem@users.noreply.github.com>
2022-09-19 12:15:04 +0500

    Add files via upload

---
+++ NewScrollView/NewScrollView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewScrollView
+//
+//  Created by Pawan iOS on 19/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var img1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
